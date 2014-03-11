98b3b6615be2b4445b39a8c67dc1a8017afc0f25
Lex Robinson <lex@playgen.com>
2014-03-11 16:06:50 +0000

    Support editing

--- civisync-options-page-editor.php
+++ civisync-options-page-editor.php
@@ -2,15 +2,29 @@
 $membershibs = civisync_get_memberships();
 $statizzles  = civisync_get_stati();
 
-	/*
-	if ( empty( $post_id ) ) {
-		wp_redirect( admin_url('post.php') );
+$rule = false;
+if ( $action != 'new' ) {
+	if ( empty( $_REQUEST['rule'] ) ) {
+		wp_redirect( admin_url('options-general.php?page=' . $_REQUEST['page']) );
 		exit();
 	}
-
-	if ( ! $post )
+	global $wpdb;
+	$rule = $wpdb->get_row( $wpdb->prepare(
+		"SELECT * FROM `{$wpdb->prefix}civi_member_sync` WHERE `id` = %d",
+		$_REQUEST['rule']
+	), ARRAY_A );
+	if ( ! $rule )
 		wp_die( __( 'You attempted to edit an item that doesn&#8217;t exist. Perhaps it was deleted?' ) );
-	*/
+}
+if ( ! $rule ) {
+	$rule = array(
+		'civi_mem_type' => '',
+		'wp_role' => '',
+		'expire_wp_role' => '',
+		'current_rule' => 'a:0:{}',
+	);
+}
+$rule['current_rule'] = unserialize( $rule['current_rule'] );
 
 function start_row( $id, $label )
 {
@@ -47,21 +61,13 @@ function do_flags( $id, array $flags, array $selected = array() )
 ?>
 	<fieldset id="<?= $id; ?>" required>
 <?php foreach( $flags as $flag_id => $name ): $cbox_name = $id . '[' . $flag_id . ']';?>
-		<input type="checkbox" name="<?= $cbox_name ?>" value="<?= $flag_id ?>">
+		<input type="checkbox" name="<?= $cbox_name ?>" value="<?= $flag_id ?>" <?php if ( ! empty( $selected[ $flag_id ] ) ) echo 'checked'; ?>>
 		<label for="<?= $cbox_name ?>"><?= $name; ?></label>
 		<br>
 <?php endforeach; ?>
 	</fieldset>
 <?php
 }
-/* <input id="<?= $id; ?>" name="<?= $id; ?>" type="<?= $type; ?>" required value="<?= $value; ?>"> */
-/*
-			'name'           => __( 'Civi Membership Type', 'civisync' ),
-			'wp_role'        => __( 'Wordpress Role', 'civisync' ),
-			'expire_wp_role' => __( 'Expiry Assign Role', 'civisync' ),
-			'current_rule'   => __( 'Current Codes', 'civisync' ),
-			'expiry_rule'    => __( 'Expired Codes', 'civisync' )
-*/
 global $wp_roles;
 $roles = $wp_roles->get_names();
 ?>
@@ -76,21 +82,29 @@ $roles = $wp_roles->get_names();
 		 you will need to add a second association rule after you have completed this one.
 	</p>
 	<form method="post" action="?page=<?php echo $_REQUEST['page']; ?>">
-		<?php wp_nonce_field( 'civisync-new-rule' ); ?>
-		<input type="hidden" name="action" value="post-new">
+		<?php
+			if ( $action == 'new' ) {
+				wp_nonce_field( 'civisync-rule-new' );
+				echo '<input type="hidden" name="action" value="post-new">';
+			} else {
+				wp_nonce_field( 'civisync-rule-edit' );
+				echo '<input type="hidden" name="action" value="post-edit">';
+			}
+		?>
+
 		<table class="form-table">
 		<?php
-			start_row( 'civi_member_type', __( 'Civi Membership Type', 'civisync' ) );
-			do_dropdown( 'civi_member_type', $membershibs );
+			start_row( 'civi_mem_type', __( 'Civi Membership Type', 'civisync' ) );
+			do_dropdown( 'civi_mem_type', $membershibs, $rule['civi_mem_type'] );
 			end_row( '' );
 			start_row( 'wp_role', __( 'Active Wordpress Role', 'civisync' ) );
-			do_dropdown( 'wp_role', $roles );
+			do_dropdown( 'wp_role', $roles, $rule['wp_role'] );
 			end_row( 'What role this membership grants when active.' );
-			start_row( 'expired_wp_role', __( 'Expired Wordpress Role', 'civisync' ) );
-			do_dropdown( 'expired_wp_role', $roles, false, false );
+			start_row( 'expire_wp_role', __( 'Expired Wordpress Role', 'civisync' ) );
+			do_dropdown( 'expire_wp_role', $roles, $rule['expire_wp_role'], false );
 			end_row( '(optional) What role a user should be given if their membership expires.' );
 			start_row( 'activation_rules', __( 'Active Statuses', 'civisync' ) );
-			do_flags( 'activation_rules', $statizzles );
+			do_flags( 'activation_rules', $statizzles, $rule['current_rule'] );
 			end_row( __( 'A membership with one of these statuses is considered to be active, otherwise it is inactive.', 'civisync' ) );
 		?>
 		</table>
